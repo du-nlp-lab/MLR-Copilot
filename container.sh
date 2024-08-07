e7e0e492053fa04bb7f5869423e3f3a1dee5a61f
Teerth Patel <trp210003@utdallas.edu>
2024-08-07 00:13:55 -0500

    Revamp docker build and container.sh

--- container.sh
+++ container.sh
@@ -1,1 +1,1 @@
-docker run -it --gpus all -v $(pwd):/app --env-file .env --user root 'tortcode/autoresearch:5.0' bash
+docker run -it --gpus all -v $(pwd):/app --env-file .env --user root 'tortcode/nlp-coresearcher:latest' bash
